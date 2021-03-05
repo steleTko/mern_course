7cca08c58d20b7b4581f237e8e19c53a6d14b591
steleTko <winterasaut2013@mail.ru>
2021-03-05 16:48:23 +0500

    "Add Frontend"

---
+++ client/src/hooks/http.hook.js
@@ -0,0 +1,42 @@
+import {useState, useCallback} from 'react'
+
+
+export const useHttp = () => {
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+    const request = useCallback(async (url, method = 'GET', body = null, headers = {}) => {
+        setLoading(true)
+        try {
+            // Приводим тело запроса к строке
+            if(body) {
+                body = JSON.stringify(body)
+                headers['Content-Type'] = 'application/json'
+            }
+            // Отправляем запрос и распарсиваем его в json
+            const response = await fetch(url, { method, body, headers })
+            const data = await response.json()
+
+            if(!response.ok) {
+                // Отправляем ошибку с сервера или с фронта
+                throw new Error( data.message || 'Что-то пошло не так')
+            }
+
+            setLoading(false)
+
+            return data
+        }
+        catch (e) {
+            setLoading(false)
+            // Ошибка прилетает с сервера
+            setError(e.message)
+            throw e
+        }
+
+    }, [])
+
+    // Чистим ошибки
+    const clearError = useCallback(() => setError(null), [])
+
+    return { loading, request, error, clearError}
+
+}
